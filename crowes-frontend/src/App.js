826dae1c7b72ff236a901ba2fd8993223db4a85c
Andrew Carrigan <carrigan223@gmail.com>
2021-02-17 14:52:41 -0800

    added router

--- crowes-frontend/src/App.js
+++ crowes-frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
 import data from "./data.js";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   const openMenu = () => {
@@ -11,59 +14,63 @@ function App() {
   };
 
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Crowes Colectibles</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Football Cards</a>
-          </li>
-          <li>
-            <a href="index.html">Baseball Cards</a>
-          </li>
-        </ul>
-      </aside>
-
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            {data.products.map((product) => (
-              <li>
-                <div className="product">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                    alt="product"
-                  />
-                  <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">{product.price}</div>
-                  <div className="product-rating">
-                    {product.rating} stars ({product.numReviews})
-                  </div>
-                </div>
-              </li>
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Crowes Colectibles</a>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
+            <li>
+              <a href="index.html">Football Cards</a>
+            </li>
+            <li>
+              <a href="index.html">Baseball Cards</a>
+            </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">All rights reserved</footer>
-    </div>
+        </aside>
+
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            <ul className="products">
+              {data.products.map((product) => (
+                <li>
+                  <div className="product">
+                    <img
+                      className="product-image"
+                      src={product.image}
+                      alt="product"
+                    />
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">{product.price}</div>
+                    <div className="product-rating">
+                      {product.rating} stars ({product.numReviews})
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </main>
+        <footer className="footer">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
