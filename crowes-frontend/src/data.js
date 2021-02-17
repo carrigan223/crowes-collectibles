edbd2826d307f43512763cc7ca6936187461feef
Andrew Carrigan <carrigan223@gmail.com>
2021-02-17 14:39:50 -0800

    added some dynamic data

---
+++ crowes-frontend/src/data.js
@@ -0,0 +1,33 @@
+const data = {
+  products: [
+    {
+      name: "Chase Young Card",
+      image: "/images/d1.jpg",
+      category: "Football",
+      price: 100,
+      brand: "score",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: "Gleyber Torres Card",
+      category: "Baseball",
+      image: "/images/d1.jpg",
+      price: 120,
+      brand: "score",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: "Chase Young Card",
+      image: "/images/d1.jpg",
+      category: "Football",
+      price: 100,
+      brand: "score",
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+};
+
+export default data;
